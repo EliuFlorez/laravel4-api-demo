adb689f0950c94a1691addfeea0714dcb6d4bcda
Maxime Beaudoin <firalabs@gmail.com>
2013-12-17 20:14:44 -0500

    Updated user caching

--- app/src/App/Resource/Caching/User.php
+++ app/src/App/Resource/Caching/User.php
@@ -10,7 +10,16 @@ class User implements CachingInterface
 
 	public function change($id = null)
 	{
+		//Forget or flush the cache of the current resource
 		Cache::forget('user.' . $id);
 		Cache::forget('users');
+	
+		// If we have to flush or forget cache from other resource
+		// Just call the specific resource caching directly here.
+		// For example : Caching::place()->change($placeId);
+		
+		//You can also flush tag here
+		// For example : Cache::tags('users')->flush();
+		
 	}
 }
